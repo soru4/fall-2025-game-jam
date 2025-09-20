3f08804e38ca1b8baaa2f7fb86832331fb03d8bc
soru4 <73024932+soru4@users.noreply.github.com>
2025-09-20 10:03:40 -0500

    basic script setup

--- Fall2025GameJam/Assets/Scripts/GameManager.cs
+++ Fall2025GameJam/Assets/Scripts/GameManager.cs
@@ -1,9 +1,21 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	public static GameManager inst; 
+	int day = 0; // 0, 1, 2, 3, 4, 5. 
+	int hour = 0; // 0 - 24
+	
+
+	public QuestTNode currentTNode; 
+    
+	// Awake is called when the script instance is being loaded.
+	protected void Awake()
+	{
+		inst = this; 
+	}
+	void Start()
     {
         
     }
@@ -11,6 +23,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+	    CheckHour();
     }
+	void CheckHour(){
+		day += hour > 24 ? 1 : 0;  
+		hour = hour > 24 ? 0 : hour; 
+		
+	}
+	//Syntax in questTNode: 0.3 (percentage of day to elapse) : 0.1 (decimal percentage of tiredness to increase)
+	
+	public void parseQuestInteractions(string x){
+		string[] diff = x.Split(":"); 
+		hour += (int)(double.Parse(diff[0]) * 24); 
+		//deal with tiredness later. 
+		
+		
+	}
 }
---
+++ Fall2025GameJam/Assets/Scripts/QuestManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+	
+	public static QuestManager inst; 
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	// Awake is called when the script instance is being loaded.
+	protected void Awake()
+	{
+		inst = this; 
+	}
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	[System.Serializable]
+	public struct Dialogue{
+		public string text; 
+		public AudioClip voiceLine; 
+		public int dayTriggered; 
+	}    
+}
+
+[System.Serializable]
+public class QuestTNode{
+	public QuestManager.Dialogue currentDialogue;
+	public string playerResponse1; 
+	public string playerResponse2; 
+	public QuestTNode response1Next; 
+	public QuestTNode response2Next; 
+	
+	public QuestTNode next; 
+	
+	public QuestTNode(QuestManager.Dialogue x, string res1, string res2, QuestTNode res1N = null, QuestTNode res2N=null, QuestTNode n = null){
+		this.currentDialogue = x; 
+		this.playerResponse1 = res1;
+		this.playerResponse2 = res2;
+		this.response1Next = res1N;
+		this.response2Next = res2N; 
+		this.next = n; 
+	}
+	
+}
+
